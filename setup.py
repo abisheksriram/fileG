c08773e40f271d1684f225b55e2c67f9a356799e
abishek Sriram <abisheksriram2001@gmail.com>
2020-09-17 07:06:16 +0530

    Files removed

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="fileG", # Replace with your own username
 
-    version="0.0.4.dev1",
+    version="0.0.6.dev1",
 
 
     author="Abishek Sriram",
